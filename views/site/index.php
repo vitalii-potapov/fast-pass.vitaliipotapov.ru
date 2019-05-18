2ac52e51f2e595cc4eee9693a874289104fa769f
VitaliiPotapov <vitalii.potapov92@gmail.com>
2019-05-19 01:59:20 +0600

    Change style fast-pass btn

--- views/site/index.php
+++ views/site/index.php
@@ -32,9 +32,58 @@ $icons = [
 <div class="site-index">
   <div class="row">
     <div class="aside-content col-xs-2">
-      <a href="javascript:(function(){let key=prompt('Ключ?',''),arrKey=key.split(''),summKey=0,arrlog=[],arrpass=[];for(let i=0;i<arrKey.length;i++){summKey+=arrKey[i].charCodeAt()} let s=window.location.search,login=s.substring(5,s.indexOf('&pass')).slice(1).split(','),pass=s.substring(s.indexOf('&pass'),s.indexOf('&attr')).slice(6).split(',');for(let i=0;i<login.length;i++){arrlog.push(String.fromCharCode(login[i]-summKey))} for(let i=0;i<pass.length;i++){arrpass.push(String.fromCharCode(pass[i]-summKey))} let attr=s.substring(s.indexOf('&attr')).slice(6).split(','),field_login=attr[1],field_pass=attr[2];if(attr[0]==0){document.querySelector(`[name='${field_login}']`).value=arrlog.join('');document.querySelector(`[name='${field_pass}']`).value=arrpass.join('');document.querySelector(`[name='${field_pass}']`).form.submit()}else{document.querySelector('#'+field_login).value=arrlog.join('');document.querySelector('#'+field_pass).value=arrpass.join('');document.querySelector('#'+field_pass).form.submit()}}())">
+      <a href="javascript:(function(){const a=document.createElement('div');a.style.cssText='position:fixed;top:0;right:0;left:0;padding:15px 0;background:black;text-align:center;z-index:1000000;box-sizing:border-box;',a.setAttribute('id','secret-key-bar');const b=document.createElement('input');b.setAttribute('type','password'),b.style.cssText='display:inline-block;width:210px;height:34px;padding:6px 12px;font-size:14px;line-height:1;color:rgb(85,85,85);background-color:rgb(255,255,255);background-image:none;border:1px solid rgb(204,204,204);border-radius:4px;margin-left:50px;box-sizing:border-box;font-family:sans-serif!important;',b.setAttribute('type','password'),b.setAttribute('placeholder','Please enter your secret key'),b.addEventListener('change',function(){let a=this.value,b=a.split(''),c=0,d=[],e=[];for(let a=0;a<b.length;a++)c+=b[a].charCodeAt();const f=window.location.search,g=f.substring(5,f.indexOf('&pass')).slice(1).split(','),h=f.substring(f.indexOf('&pass'),f.indexOf('&attr')).slice(6).split(',');for(let a=0;a<g.length;a++)d.push(String.fromCharCode(g[a]-c));for(let a=0;a<h.length;a++)e.push(String.fromCharCode(h[a]-c));const i=f.substring(f.indexOf('&attr')).slice(6).split(','),j=i[1],k=i[2];0==i[0]?(console.log(document.querySelector(`[name='${j}']`)),console.log(document.querySelector(`[name='${k}']`)),document.querySelector(`[name='${j}']`).value=d.join(''),document.querySelector(`[name='${k}']`).value=e.join(''),document.querySelector(`[name='${k}']`).form.submit()):(document.querySelector('#'+j).value=d.join(''),document.querySelector('#'+k).value=e.join(''),document.querySelector('#'+k).form.submit())});const c=document.createElement('span');c.style.cssText='display:inline-block;padding:7px 9.7px;margin-left:10px;border:2px solid white;border-radius:3px;color:white;font-weight:700;cursor:pointer;box-sizing:border-box;line-height:1;font-family:sans-serif!important;font-size:16px;',c.innerText='X',c.addEventListener('click',function(){document.querySelector('#secret-key-bar').remove()}),a.appendChild(b),a.appendChild(c),document.querySelector('body').appendChild(a),b.focus()})();">
         Fast-pass
       </a>
+      <!-- ====== Исходный код javascirpta start ======
+      javascript:(function(){
+        const myElement = document.createElement('div');
+        myElement.style.cssText = 'position:fixed;top:0;right:0;left:0;padding:15px 0;background:black;text-align:center;z-index:1000000;box-sizing:border-box;';
+        myElement.setAttribute('id', 'secret-key-bar');
+        const input = document.createElement('input');
+        input.setAttribute('type', 'password');
+        input.style.cssText = 'display:inline-block;width:210px;height:34px;padding:6px 12px;font-size:14px;line-height:1;color:rgb(85,85,85);background-color:rgb(255,255,255);background-image:none;border:1px solid rgb(204,204,204);border-radius:4px;margin-left:50px;box-sizing:border-box;font-family:sans-serif!important;';
+        input.setAttribute('type', 'password');
+        input.setAttribute('placeholder', 'Please enter your secret key');
+        input.addEventListener('change', function(){
+          let key = this.value,
+              arrKey=key.split(''),
+              summKey=0,
+              arrlog=[],
+              arrpass=[];
+          for(let i=0;i<arrKey.length;i++){summKey+=arrKey[i].charCodeAt()};
+          const s=window.location.search,
+              login=s.substring(5,s.indexOf('&pass')).slice(1).split(','),
+              pass=s.substring(s.indexOf('&pass'),s.indexOf('&attr')).slice(6).split(',');
+          for(let i=0;i<login.length;i++){arrlog.push(String.fromCharCode(login[i]-summKey))};
+          for(let i=0;i<pass.length;i++){arrpass.push(String.fromCharCode(pass[i]-summKey))};
+          const attr=s.substring(s.indexOf('&attr')).slice(6).split(','),
+              field_login=attr[1],
+              field_pass=attr[2];
+          if(attr[0]==0){
+            console.log(document.querySelector(`[name='${field_login}']`));
+            console.log(document.querySelector(`[name='${field_pass}']`));
+            document.querySelector(`[name='${field_login}']`).value=arrlog.join('');
+            document.querySelector(`[name='${field_pass}']`).value=arrpass.join('');
+            document.querySelector(`[name='${field_pass}']`).form.submit()
+          }else{
+            document.querySelector('#'+field_login).value=arrlog.join('');
+            document.querySelector('#'+field_pass).value=arrpass.join('');
+            document.querySelector('#'+field_pass).form.submit()
+          }
+        });
+        const close = document.createElement('span');
+        close.style.cssText = 'display:inline-block;padding:7px 9.7px;margin-left:10px;border:2px solid white;border-radius:3px;color:white;font-weight:700;cursor:pointer;box-sizing:border-box;line-height:1;font-family:sans-serif!important;font-size:16px;';
+        close.innerText = 'X';
+        close.addEventListener('click', function(){
+          document.querySelector('#secret-key-bar').remove();
+        });
+        myElement.appendChild(input);
+        myElement.appendChild(close);
+        document.querySelector('body').appendChild(myElement);
+        input.focus();
+      }());
+      ====== Исходный код javascirpta end ====== -->
       <br>
       <small>Move the "Fast-pass" to your panel bookmarks</small>
     </div>
